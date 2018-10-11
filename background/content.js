256259e924ad03b39b397f10ee65378e74289ba4
David Pacassi Torrico <dpacassi@gmail.com>
2018-10-11 12:22:06 +0200

    Copy over <noscript> attributes
    Fixes #43.

--- background/content.js
+++ background/content.js
@@ -6,7 +6,16 @@
   for (var i = 0; i < tags.length; i++) {
     if (tags[i].firstChild) {
       var newDiv = document.createElement('div');
+      var a = tags[i].getAttributeNames();
+
+      // Copy over <noscript> content.
       newDiv.innerHTML = tags[i].innerHTML;
+
+      // Copy over <noscript> attributes.
+      for (var k = 0; k < a.length; k++) {
+        newDiv.setAttribute(a[k], tags[i].getAttribute(a[k]));
+      }
+
       tags[i].parentNode.replaceChild(newDiv, tags[i]);
     }
   }
